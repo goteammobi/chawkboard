ed0946a2717b54a498f142ab88440e49c819d4ec
Harrison Chen <harrisonxchen@gmail.com>
2015-03-24 22:34:59 -0700

    pushing for example

--- client/templates/chatroom.js
+++ client/templates/chatroom.js
@@ -1,3 +1,13 @@
+Template.chatroom.helpers({
+  chatrooms: function() {
+    return [
+      { name: "Homies" },
+      { name: "Riverside Peeps" },
+      { name: "Maplestory Guild" }
+    ]
+  }
+});
+
 Template.chatroom.events({
   'click .science-btn': function() {
     alert("SCIENCE BITCH!");
